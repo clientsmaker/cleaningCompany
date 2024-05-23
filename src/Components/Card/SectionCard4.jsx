1ab7f9cd85a3aae8edd5c1873680079f0dab04e4
clientsmaker <interiorclientsmaker@gmail.com>
2024-05-23 11:36:50 +0530

    change logo

--- src/Components/Card/SectionCard4.jsx
+++ src/Components/Card/SectionCard4.jsx
@@ -8,7 +8,7 @@ const SectionCard4 = () => {
           <div className="grid grid-cols-1 divide-y text-center dark:divide-gray-700/75 sm:grid-cols-3 sm:divide-x sm:divide-y-0">
             <dl className="space-y-1 px-5 py-8">
               <dt className="text-4xl font-extrabold text-black dark:text-black">
-                80+
+                500+
               </dt>
               <dd className="text-sm font-semibold uppercase tracking-wide text-blue-600 dark:text-blue-500">
                 Projects
@@ -16,7 +16,7 @@ const SectionCard4 = () => {
             </dl>
             <dl className="space-y-1 px-5 py-8">
               <dt className="text-4xl font-extrabold text-black dark:text-black">
-                100+
+                1100+
               </dt>
               <dd className="text-sm font-semibold uppercase tracking-wide text-blue-600 dark:text-blue-500">
                 Clients
@@ -24,7 +24,7 @@ const SectionCard4 = () => {
             </dl>
             <dl className="space-y-1 px-5 py-8">
               <dt className="text-4xl font-extrabold text-black dark:text-black">
-                10+
+                100+
               </dt>
               <dd className="text-sm font-semibold uppercase tracking-wide text-blue-600 dark:text-blue-500">
                Services
